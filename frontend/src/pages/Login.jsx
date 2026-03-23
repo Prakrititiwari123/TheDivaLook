092865745fbb88c2425840b2d6254660c65fbf74
Prakrititiwari123 <anuragtiwaribadkhara@gmail.com>
2026-03-23 16:28:35 +0530

    modifying the diva look

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
